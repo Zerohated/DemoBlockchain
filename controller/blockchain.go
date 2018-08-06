87959a85d765d4c601a4654bf9e48465f450f45f
David Chou <david.chou93@gmail.com>
2018-08-06 18:35:22 +0800

    remove dep support

--- controller/blockchain.go
+++ controller/blockchain.go
@@ -3,53 +3,43 @@ package controller
 import (
 	"DemoBlockchain/model"
 	"encoding/json"
-	"io"
-	"net/http"
+	"fmt"
+
+	"github.com/kataras/iris"
 
 	"github.com/davecgh/go-spew/spew"
 )
 
-func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	response, err := json.MarshalIndent(payload, "", "  ")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("HTTP 500: Internal Server Error"))
-		return
-	}
-	w.WriteHeader(code)
-	w.Write(response)
-}
-
-func HandleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+func HandleGetBlockchain(ctx iris.Context) {
 	bytes, err := json.MarshalIndent(model.Blockchain, "", "  ")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		ctx.StatusCode(iris.StatusInternalServerError)
 		return
 	}
-	io.WriteString(w, string(bytes))
+	ctx.Writef(string(bytes))
 }
-func HandleWriteBlock(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var m model.Message
 
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&m); err != nil {
-		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+func HandleWriteBlock(ctx iris.Context) {
+	ctx.Header("Content-Type", "application/json")
+	var message model.Message
+	if err := ctx.ReadJSON(&message); err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.WriteString(err.Error())
 		return
 	}
-	defer r.Body.Close()
 
-	model.Mutex.Lock()
-	newBlock := model.GenerateBlock(model.Blockchain[len(model.Blockchain)-1], m.Info)
-	model.Mutex.Unlock()
+	fmt.Printf("Received: %#+v\n", message)
+
+	// model.Mutex.Lock()
+	newBlock := model.GenerateBlock(model.Blockchain[len(model.Blockchain)-1], message.Info)
+	// model.Mutex.Unlock()
 
 	if model.IsBlockValid(newBlock, model.Blockchain[len(model.Blockchain)-1]) {
 		newBlockchain := append(model.Blockchain, newBlock)
 		model.ReplaceChain(newBlockchain)
 		spew.Dump(model.Blockchain)
 	}
-
-	respondWithJSON(w, r, http.StatusCreated, newBlock)
+	ctx.StatusCode(iris.StatusCreated)
+	ctx.JSON(newBlock)
 
 }
